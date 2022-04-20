85b0967beebfef0780510fdd3cdac2b6568f21ea
David McFarland <davidomcfarland@gmail.com>
2022-04-19 19:02:38 -0600

    inital commit

---
+++ src/views/Tab2Page.vue
@@ -0,0 +1,67 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Photo Gallery</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Photo Gallery</ion-title>
+        </ion-toolbar>
+      </ion-header>
+
+      <ion-fab vertical="center" horizontal="center">
+        <ion-fab-button @click="takePhoto()">
+          <ion-icon :icon="camera"></ion-icon>
+        </ion-fab-button>
+      </ion-fab>
+      
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+
+import { camera, trash, close } from 'ionicons/icons';
+import {
+  IonPage,
+  IonHeader,
+  IonFab,
+  IonFabButton,
+  IonIcon,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonImg,
+} from '@ionic/vue';
+
+export default {
+  name: 'PhotoGallery',
+  components: {
+    IonPage,
+    IonHeader,
+    IonFab,
+    IonFabButton,
+    IonIcon,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    //IonGrid,
+    //IonRow,
+    //IonCol,
+    //IonImg,
+  },
+  setup() {
+    return {
+      camera,
+      trash,
+      close,
+    };
+  },
+};
+</script>
